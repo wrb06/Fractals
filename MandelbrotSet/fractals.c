ad3f0417bfa24bb8f78990b995793c170ba463ef
wrb06 <44703594+wrb06@users.noreply.github.com>
2020-02-29 19:08:19 +0000

    Added Mandelbrot Set generator

---
+++ MandelbrotSet/fractals.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+const int maxLoops = 100;
+
+int main(){
+  FILE *fp;
+  fp=fopen("test.txt", "w");
+  if(fp == NULL)
+      return 1;
+
+  int x = 0;
+  double zr, zc, znr, znc, znrn, zncn;
+
+  for (double y = -1; y <= 1; y+=0.0001) {
+    if (x%100 == 0){
+      printf("%d\n", x/100);
+    }
+    for (double x = -1; x <= 1; x+=0.0001) {
+      zr = x;
+      zc = y;
+      znr = x;
+      znc = y;
+
+      int i = 0;
+      while (i < maxLoops && (znr*znr + znc*znc) < 10){
+        znrn = znr*znr - znc*znc + zr;
+        zncn = 2 * znr * znc + zc;
+        znr = znrn;
+        znc = zncn;
+        i++;
+      }
+      if (i>=100){
+        fprintf(fp, "%d", 9);
+      } else {
+        fprintf(fp, "%d", 0);
+      }
+    }
+    x++;
+    fprintf(fp, "\n");
+  }
+
+
+  fclose(fp);
+  return 0;
+}
---
+++ MandelbrotSet/visualiser.py
@@ -0,0 +1,20 @@
+from PIL import Image, ImageDraw
+
+img = Image.new('RGB', (20000, 20000), color = "black")
+d = ImageDraw.Draw(img)
+
+f = open("test.txt", "r")
+x = 0
+y = 0
+for line in f:
+    for char in line:
+        if char == "9":
+            d.point([(x, y)], "white")
+        x+=1;
+    x=0
+    y+=1
+    if (y%100 == 0):
+        print(y)
+
+
+img.save('image.png')
